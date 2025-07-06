f2c60f6db5b0c22b9da6827009b5988515ba85f3
blipee-dev <pedro@blipee.com>
2025-07-06 21:39:43 +0000

    Fix TypeScript error: handle null canvas in AmbientBackground

--- src/components/effects/AmbientBackground.tsx
+++ src/components/effects/AmbientBackground.tsx
@@ -28,9 +28,9 @@ export function AmbientBackground() {
       opacity: number
       color: string
 
-      constructor() {
-        this.x = Math.random() * canvas.width
-        this.y = Math.random() * canvas.height
+      constructor(canvasWidth: number, canvasHeight: number) {
+        this.x = Math.random() * canvasWidth
+        this.y = Math.random() * canvasHeight
         this.size = Math.random() * 2 + 0.5
         this.speedX = (Math.random() - 0.5) * 0.5
         this.speedY = (Math.random() - 0.5) * 0.5
@@ -38,14 +38,14 @@ export function AmbientBackground() {
         this.color = Math.random() > 0.5 ? '#8B5CF6' : '#0EA5E9'
       }
 
-      update() {
+      update(canvasWidth: number, canvasHeight: number) {
         this.x += this.speedX
         this.y += this.speedY
 
-        if (this.x > canvas.width) this.x = 0
-        if (this.x < 0) this.x = canvas.width
-        if (this.y > canvas.height) this.y = 0
-        if (this.y < 0) this.y = canvas.height
+        if (this.x > canvasWidth) this.x = 0
+        if (this.x < 0) this.x = canvasWidth
+        if (this.y > canvasHeight) this.y = 0
+        if (this.y < 0) this.y = canvasHeight
       }
 
       draw() {
@@ -67,7 +67,7 @@ export function AmbientBackground() {
 
     // Initialize particles
     for (let i = 0; i < particleCount; i++) {
-      particles.push(new Particle())
+      particles.push(new Particle(canvas.width, canvas.height))
     }
 
     // Animation loop
@@ -75,7 +75,7 @@ export function AmbientBackground() {
       ctx.clearRect(0, 0, canvas.width, canvas.height)
       
       particles.forEach(particle => {
-        particle.update()
+        particle.update(canvas.width, canvas.height)
         particle.draw()
       })
       
